c78a35cb3e27f594dfe10be17765709081446506
lelouchZhang <zhch8128@gmail.com>
2026-05-28 16:07:31 +0800

    feat: init project wth tanstack-start

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "@tanstack/react-router";
+import { LogIn } from "lucide-react";
+
+const Navbar = () => (
+	<nav className="navbar">
+		<div className="brand">
+			<div className="mark">
+				<div className="glyph" />
+			</div>
+			<Link to="/">
+				<span>Skills</span>
+			</Link>
+		</div>
+		<div className="actions">
+			{/* <Show when="signed-in">
+      <UserButton/>
+      </Show> */}
+			{/* <Show when="signed-out"> */}
+			<Link to="/sign-in/$" className="btn-primary">
+				<LogIn size={16} />
+				Sign in
+			</Link>
+			{/* </Show> */}
+		</div>
+	</nav>
+);
+
+export default Navbar;
